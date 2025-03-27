2fb07cd2dafd7ead5d9db4c5ef67ef6036e0002b
hungerbk <wjantlr@gmail.com>
2025-03-28 02:58:33 +0900

    💬 [Comment] emotion.d.ts에 interface 관련 주석 추가

--- src/emotion.d.ts
+++ src/emotion.d.ts
@@ -6,3 +6,15 @@ declare module "@emotion/react" {
     colors: { primary: string; black001: string; gray001: string; gray002: string };
   }
 }
+
+/* 
+NOTE:
+declare module "@emotion/react" {
+  export interface Theme {}
+}
+
+Emotion의 타입 시스템은 내부적으로 위처럼 정의돼 있음
+
+따라서 우리가 만든 ThemeType으로 Emotion 내부의 Theme을 보강(Declaration Merging) 하려면 interface 확장만 가능.
+type으로 재정의하거나 Theme = ThemeType으로 하면 병합이 아니라 오버라이드라서 제대로 동작하지 않거나, 타입 에러가 발생할 수 있음.
+ */
