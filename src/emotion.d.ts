730f8bd4a93a01d30f75c624213e5c850aac5116
hungerbk <wjantlr@gmail.com>
2025-03-28 02:50:11 +0900

    🏷️ [Type] emotion.d.ts Theme interface 확장

--- src/emotion.d.ts
+++ src/emotion.d.ts
@@ -2,5 +2,7 @@ import "@emotion/react";
 import { ThemeType } from "./styles/theme";
 
 declare module "@emotion/react" {
-  export type Theme = ThemeType;
+  export interface Theme extends ThemeType {
+    colors: { primary: string; black001: string; gray001: string; gray002: string };
+  }
 }
