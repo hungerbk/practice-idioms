4ab21c01ef510d8f2cb087c75559c1b3fa3addc3
hungerbk <wjantlr@gmail.com>
2025-03-28 02:24:28 +0900

    💄 [Design] 문제 개수 입력 input 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,40 @@ const SelectForm = styled.form`
   background-color: gray;
 `;
 
+const SelectSpan = styled.span`
+  width: 50%;
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 0.5rem;
+
+  input {
+    width: 100%;
+    height: 2.5rem;
+    border: 2px solid ${({ theme }) => theme.colors.gray001};
+    border-radius: 8px;
+    font-size: 18px;
+    text-align: center;
+  }
+
+  p {
+    color: ${({ theme }) => theme.colors.gray002};
+    padding-left: 1rem;
+  }
+`;
+
 function App() {
   return (
     <>
       <SelectForm>
         <h1>사자성어 연습 게임</h1>
         <RadioButtonGroup />
-        <span>
-          <label htmlFor="number-of-problem">문제 수</label>
-          <input type="number" max={20} min={5} id="number-of-problem" required />
-          <p>5부터 20까지의 숫자를 선택해주세요.</p>
-        </span>
+        <SelectSpan>
+          <label htmlFor="number-of-problem" className="a11y-hidden">
+            문제 수
+          </label>
+          <input type="number" max={20} min={5} id="number-of-problem" placeholder="문제 수를 입력해 주세요" required />
+          <p>* 5부터 20까지의 숫자를 입력해주세요.</p>
+        </SelectSpan>
         <button type="submit">시작</button>
       </SelectForm>
 
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,6 +3,7 @@ export const theme = {
     primary: "#ffe4c4",
     black001: "#333",
     gray001: "#ccc",
+    gray002: "#a9a9a9",
   },
 };
 
