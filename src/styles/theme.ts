21d5c00008280a8eac29971ae341d39f9af54efe
hungerbk <wjantlr@gmail.com>
2025-03-28 02:57:28 +0900

    🏷️ [Type] theme에 as const 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,6 +5,6 @@ export const theme = {
     gray001: "#ccc",
     gray002: "#a9a9a9",
   },
-};
+} as const; //NOTE: as const를 붙여야 colors.primary 같은 속성들이 리터럴 타입으로 고정돼서 타입 안정성과 자동완성에 유리
 
 export type ThemeType = typeof theme;
